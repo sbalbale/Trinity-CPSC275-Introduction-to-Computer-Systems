f5212c5f3251f30d7c0d5ac9a06f1e3851065938
Sean Balbale <sean.balbale@gmail.com>
2025-09-03 14:09:05 -0400

    Add lab01 hello and num programs
    Added hello.c and num.c source files for lab01, along with their compiled binaries and input file. hello.c prints a greeting, while num.c reads two numbers and outputs their sum.

---
+++ lab01/hello.c
@@ -0,0 +1,7 @@
+// hello.c
+#include <stdio.h>
+
+void main(){
+	printf("hello, CPSC 275!\n");
+}
+
---
+++ lab01/num.c
@@ -0,0 +1,15 @@
+/**
+* num.c
+* Sean Balbale
+* CPSC 275
+**/
+
+#include <stdio.h>
+
+void main()
+{
+  int num1,num2;
+  printf("Enter two numbers seperated by a space \n");
+  scanf("%d%d",&num1,&num2);
+  printf("The sum is %d",num1+num2);
+}
