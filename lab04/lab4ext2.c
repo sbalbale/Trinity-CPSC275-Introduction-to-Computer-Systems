8f660382af9696812a0775ce2b28497d2e872a9b
Balbale <sbalbale@netlab11.cs.trincoll.edu>
2025-09-24 14:03:02 -0400

    lab

--- lab04/lab4ext2.c
+++ lab04/lab4ext2.c
@@ -5,12 +5,14 @@ void func1(int xval)
    int x;
    x = xval;
    /* print the address and value of x here */
+   printf("x) value:%i , address: %p \n",x,(void*)&x);
 }
 
 void func2(int dummy)
 {
    int y;
    /* print the address and value of y here */
+   printf("y) value:%i, address: %p \n",y,(void*)&y);
 }
 
 void main()
